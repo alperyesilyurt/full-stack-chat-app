927e61db8d0b3a30917e91443084c007bd9dc3b0
Alper Yesilyurt <alperesilyurt@hotmail.com>
2022-10-12 13:44:50 +0300

    delete folder

--- server/index.js
+++ server/index.js
@@ -8,6 +8,16 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(()=>{
+  console.log("DB Connection Succesful")
+}).catch((err)=>{
+  console.log(err.message)
+});
+
+
 const server = app.listen(process.env.PORT, () => {
   console.log(`Server started on Port ${process.env.PORT}`);
 });
